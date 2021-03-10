24db342e7b66f9b7e33aff1e06ab22df791c64d4
Hillary Kitoyo <78082802+Hillary2021@users.noreply.github.com>
2021-03-10 15:17:21 +0300

    Add files via upload

---
+++ Python args.py
@@ -0,0 +1,13 @@
+def my_function():
+   print("Hello world")
+  #arguments
+def sum(x,y):
+  a=x+y
+  print ("The sum is",a)
+sum(10,20)
+sum(501,958)
+#sum
+def sum(x,y):
+  a=x+y
+  return a
+print(sum(10,20))
